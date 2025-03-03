50a39c974a21723dcf54ecece9bca0b7350c0288
Wh1isper <9573586@qq.com>
2025-03-03 22:16:40 +0800

    Add figma to toolbox

--- mcp_toolbox/config.py
+++ mcp_toolbox/config.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
+
+HOME = Path("~/.zerolab/mcp-toolbox").expanduser()
 
 
 class Config(BaseSettings):
     figma_api_key: str | None = None
+
+    cache_dir: str = (HOME / "cache").expanduser().resolve().absolute().as_posix()
 
 
 if __name__ == "__main__":
