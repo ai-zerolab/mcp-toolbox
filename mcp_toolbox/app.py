0dc848ff377014e193b5adfdbf944756b15fb6fb
Wh1isper <9573586@qq.com>
2025-03-04 09:30:35 +0800

    feat: Expend user path

--- mcp_toolbox/app.py
+++ mcp_toolbox/app.py
@@ -6,3 +6,6 @@ mcp = FastMCP("email")
 import mcp_toolbox.command_line.tools  # noqa: E402
 import mcp_toolbox.figma.tools  # noqa: E402
 import mcp_toolbox.file_ops.tools  # noqa: E402, F401
+
+
+# TODO: Add prompt for toolbox's tools
